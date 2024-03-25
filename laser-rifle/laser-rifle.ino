38ebdf61e1bcea86655afcb59d5315552b53a8fc
Federico Berlanda <berlandafederico@gmail.com>
2024-03-25 22:56:29 +0100

    update radio function and other adjustment

--- laser-rifle/laser-rifle.ino
+++ laser-rifle/laser-rifle.ino
@@ -7,63 +7,110 @@
 // radio pinout
 int CE = 9;
 int CSN = 8;
-const byte address[6] = "00011";
+const byte slaveAddress[5] = {'R', 'x', 'A', 'A', 'A'};
+
 RF24 radio(CE, CSN);
 
 // laser and trigger pinout
 int laser_duration= 250; //ms
-int laser_pin = 1;
-int trigger_pin = 2;
+int laser_pin = 2;
+int trigger_pin = 3  ;
+int value_zero;
 
 // led ammo
-int led_ammo[5]={3,4,5,6,7};
+int led_ammo[5]={A0, 5, 6, 7, 10};
+
+// noise
+int noise_pin = 4;
+
+// mode selection
+int selection_pin = 13;
+int selection_zero;
 
 void setup() {
   //radio setup
   radio.begin();
-  radio.openWritingPipe(address);
-  radio.stopListening();
+  radio.setDataRate( RF24_250KBPS );
+  radio.setRetries(3, 5); // delay, count
+  radio.openWritingPipe(slaveAddress);
 
-  // trigger and laser setup
+  // trigger, noise and laser setup
+  pinMode(noise_pin, OUTPUT);
   pinMode(laser_pin, OUTPUT);
   pinMode(trigger_pin, INPUT_PULLUP);
+  value_zero = digitalRead(trigger_pin);
+
+  // mode selection
+  pinMode(noise_pin, INPUT_PULLUP);
+  selection_zero= digitalRead(selection_pin);
 
   // led ammo setup
-  for (int i=0; i<4; i++)
+  for (int i=0; i<5; i++)
     pinMode(led_ammo[i], OUTPUT);
+
+  Serial.begin(9600);
+
 }
+
 
 void loop() {
   int number_ammo=5;
+  int current_selection = digitalRead(trigger_pin);
+
+  // Race condition
+  if (current_selection == selection_zero){
+    for (int i = 0; i < number_ammo; i++)
+      digitalWrite(led_ammo[i], HIGH);
   
-  int value_zero = digitalRead(trigger_pin);
-  for (int i = 0; i < number_ammo; i++)
-    digitalWrite(led_ammo[i], HIGH);
-    
-  while (number_ammo>0){
-    set_led_amm(number_ammo, led_ammo);
-    radio.write(&number_ammo, sizeof(number_ammo));
-    if (digitalRead(trigger_pin)!=value_zero)
-      number_ammo=bang_bang(laser_pin, laser_duration, number_ammo);
+    while (number_ammo>0){
+      if (digitalRead(trigger_pin)!=value_zero){
+          number_ammo = bang_bang(laser_pin, laser_duration, number_ammo);
+          set_led_amm(number_ammo, led_ammo);
+          delay(40);
+      }
+      radio.write(&number_ammo, sizeof(number_ammo));
+      delay(10);
+    }
+    number_ammo=10;
+    for (int i=0; i<100; i++){
+      radio.write(&number_ammo, sizeof(number_ammo));
+      delay(10);
+    }
+    delay(10000);
+  }
+
+  // Easy condition
+  else{
+    while (number_ammo>0){
+      if (digitalRead(trigger_pin)!=value_zero){
+            number_ammo = bang_bang(laser_pin, laser_duration, number_ammo);
+            set_led_amm(number_ammo, led_ammo);
+            delay(40);
+        }
+        radio.write(&number_ammo, sizeof(number_ammo));
+        delay(10);
+    }
+    number_ammo=10;
+    for (int i=0; i<100; i++){
+      radio.write(&number_ammo, sizeof(number_ammo));
+      delay(10);
+    }
     delay(10);
   }
-  
-  for (int i=0; i<100; i++){
-    radio.write(&number_ammo, sizeof(number_ammo));
-    delay(10);
-  }
-  
-  delay(10000);
 }
+
+
+int bang_bang (int laser_pin, int laser_duration, int number_ammo){
+  digitalWrite(laser_pin, HIGH);
+  digitalWrite(noise_pin, HIGH);
+  delay (laser_duration);
+  digitalWrite(noise_pin, LOW);
+  digitalWrite(laser_pin, LOW);
+  return number_ammo-1;
+}
+
 
 void set_led_amm(int number_ammo, int* led_ammo){
   for (int i = number_ammo; i < 5; i++)
     digitalWrite(led_ammo[i], LOW);
-}
-
-int bang_bang (int laser_pin, int laser_duration, int number_ammo){
-  digitalWrite(laser_pin, HIGH);
-  delay (laser_duration);
-  digitalWrite(laser_pin, LOW);
-  return number_ammo-1;
 }
--- target/target.ino
+++ target/target.ino
@@ -3,6 +3,8 @@
  * observe: there are 10 sec for calibration when you start and when you reset the target
 */
 
+#define Debug true
+
 // radio library
 #include <SPI.h>
 #include <nRF24L01.h>
@@ -13,29 +15,40 @@
 // radio pinout
 int CE= 9;
 int CSN= 8;
-const byte address[6]= "00011";
+const byte thisSlaveAddress[5] = {'R','x','A','A','A'};
 RF24 radio(CE, CSN);
-int number_ammo= 0;
+int number_ammo = 0; // this is the data sent in the TX.
+bool newData = false;
+
 // target pinout
-int target_pin[6]= {A0, A1, A2, A3, A4};
-bool target_covered[6];
-int threshold_target[6];
+int target_pin[5]= {A0, A1, A2, A3, A4};
+bool target_covered[5];
+int threshold_target[5];
 int target_hit= 0;
 
 // servo pinout
 int const close_angle= 30; //counterclockwise: Positive
+int const offset_angle = 90;
 int const n_servo= 3;
 int servo_attaches[n_servo]= {3,5,6}; // they are PWM pins
 Servo myservo[n_servo];
 
+
+
 void setup() {
+  if (Debug){
+    Serial.begin(9600);
+    Serial.println("SimpleRx Starting");
+  }
+
   //radio setup
   radio.begin();
-  radio.openReadingPipe(0, address);
+  radio.setDataRate( RF24_250KBPS );
+  radio.openReadingPipe(1, thisSlaveAddress);
   radio.startListening();
 
   // target setup
-  for (int i=0; i<6; i++){
+  for (int i=0; i<5; i++){
     pinMode(target_pin[i], INPUT_PULLUP); //check it
     target_covered[i] = false;
   }
@@ -46,48 +59,74 @@ void setup() {
   reset_servo();
 
   // threshold calibration
-  for (int i=0; i<6; i++)
+  for (int i=0; i<5; i++)
     threshold_target[i]= calibration_fun(target_pin[i]);
 }
 
 void loop() {
-
-  if (radio.available())
-    radio.read(&number_ammo, sizeof(number_ammo));
   
-  for (int i=0; i<6; i++){
-    if (target_covered[i]== false){
-      if(analogRead(target_pin[i])> threshold_target[i]){
-         close_target (i+1, target_covered);
-         target_covered[i]= true;
-         target_hit=target_hit+1;
+  if (radio.available()){
+    radio.read(&number_ammo, sizeof(number_ammo));
+    newData = true;
+  }
+
+  if (Debug == true){
+    if (newData == true) {
+        Serial.print("Data received: ");
+        Serial.println(number_ammo);
+        newData = false;
+    }
+  }
+
+  if (number_ammo != 10){
+    for (int i=0; i<5; i++){
+      if (target_covered[i] == false){
+        if(analogRead(target_pin[i])> threshold_target[i]){
+           close_target (i+1, target_covered);
+           target_covered[i]= true;
+           target_hit=target_hit+1;
+        }
+      }
+    }
+  
+    /*if (Debug){
+      Serial.print("number of ammo: ");
+      Serial.println(number_ammo);
+      for (int i=0; i<5; i++){
+        Serial.print(target_covered[i]);
+        Serial.print("\t");
+      }
+      Serial.print("Target_hit: ");
+      Serial.println(target_hit);
+      Serial.println();
+    }*/
+  
+    if (target_hit == 5 || number_ammo== 10){
+      delay(2000);
+      reset_servo();
+      target_hit=0;
+      for (int i=0; i<5; i++){
+        target_covered[i] = false;
+        threshold_target[i]= calibration_fun(target_pin[i]);
       }
     }
   }
-
-  if (target_hit== 5 || number_ammo== 0){
-    reset_servo();
-    target_hit=0;
-    for (int i=0; i<6; i++){
-      target_covered[i] = false;
-      threshold_target[i]= calibration_fun(target_pin[i]);
-    }
-  }
-  delay(50);
-
+  delay(1000);
 }
+
+// Servo function
 
 void reset_servo(){
   for (int i=0; i<3; i++)
-    myservo[2].write(0);
+    myservo[i].write(offset_angle);
 }
 
 void close_target (int target, bool* target_covered){
-  if (target== 1 || target== 2){
+  if (target == 1 || target == 2){
     close_servo_target (0, target, target_covered);
     return;
   }
-  if (target== 2 || target== 3){
+  if (target == 2 || target == 3){
     close_servo_target (1, target, target_covered);
     return;
   }
@@ -98,25 +137,24 @@ void close_target (int target, bool* target_covered){
 void close_servo_target (int servo, int target, bool* target_covered){
   if (target%2== 0){
     if (target_covered[target-2]==true){
-      myservo[servo].write(2*close_angle);
+      myservo[servo].write(offset_angle+2*close_angle);
       return;
     }
-    myservo[servo].write(-close_angle);
+    myservo[servo].write(offset_angle-close_angle);
     return;
   }
   if (target_covered[target]==true){ // this target is target+1-1(array)
-    myservo[servo].write(-2*close_angle);
+    myservo[servo].write(offset_angle-2*close_angle);
     return;
   }
-  myservo[servo].write(close_angle);
+  myservo[servo].write(offset_angle+close_angle);
   return;
 }
 
+
+// Calibration function
+
 int calibration_fun(int target){
-  double d=-2.6542e-07;
-  double a=0.0006;
-  double b=-0.55;
-  double c=205;
   double sum = 0.0;
 
   for (int i =0; i<20; i++){
@@ -125,5 +163,9 @@ int calibration_fun(int target){
   }
   
   double mean=sum / 20;
-  return int(mean +d*mean*mean*mean+a*mean*mean+b*mean+c);
+  if (Debug)
+     Serial.println(int(mean));
+    
+  //return int(mean +d*mean*mean*mean+a*mean*mean+b*mean+c);
+  return mean+10;
 }
